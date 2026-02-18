57cc815ab2b6d6c0db7329ec7809bc61e66fd2c0
dev-j03l <jojanj@tcd.ie>
2026-02-18 19:50:45 +0000

    Implement Parsing
    Successfully implemented the parsing. Web Proxy now parses requests once
    set in browser. No forwarding yet.

--- ClientHandler.java
+++ ClientHandler.java
@@ -4,12 +4,15 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class ClientHandler implements Runnable {
+    // clientSocket number
     private final Socket clientSocket;
 
+    // Constructor
     public ClientHandler(Socket clientSocket){
         this.clientSocket = clientSocket;
     }
 
+    // As we implement runnable we need to override the default interface method.
     @Override
     public void run(){
         String threadName = Thread.currentThread().getName();
@@ -25,8 +28,6 @@ public class ClientHandler implements Runnable {
             String requestLine = reader.readLine();
 
             if (requestLine == null || requestLine.isEmpty()) return;
-            
-            System.out.println("Raw Request Line: "+ requestLine);
 
             String[] parts = requestLine.split(" ");
             if (parts.length < 3){
@@ -48,15 +49,56 @@ public class ClientHandler implements Runnable {
                 if (colonIndex > 0) {
                     String key = line.substring(0, colonIndex).trim();
                     String value = line.substring(colonIndex + 1).trim();
-                    headers.put(key, value);
+                    headers.put(key.toLowerCase(), value);
                 } 
             }
 
-            System.out.println("Method: " + method);
-            System.out.println("Target: "+ target);
-            System.out.println("HTTP Version: "+ httpVersion);
-            String hostHeader = headers.get("Host");
-            System.out.println("Host Header: " + hostHeader);
+
+            // Extracting the host and port from the requestLine target.
+            // There are two cases. 
+            // 1) HTTP - GET we are provided HOST, PORT (default = 80) and Path
+            // 2) HTTPS - CONNECT example.com:443 only HOST and PORT
+            String host = "";
+            int port = -1;
+            String path = "";
+
+            // CONNECT: target is usually "host:port"
+            if(method.equalsIgnoreCase("CONNECT")) {
+                String[] hp = target.split(":", 2);
+                host = hp[0];
+                port = (hp.length == 2) ? Integer.parseInt(hp[1]) : 443;
+                path = ""; // Not used for CONNECT requests
+            } else {
+                if(target.startsWith("http://")) {
+                    String url = target.substring(7); // We strip the http:// from the target
+
+                    int slashIndex = url.indexOf("/");
+                    String hostPart = url.substring(0, slashIndex);
+                    path = (slashIndex != -1) ? url.substring(slashIndex) : "/"; 
+
+                    String[] hp = hostPart.split(":", 2);
+                    host = hp[0];
+                    port = (hp.length == 2) ? Integer.parseInt(hp[1]) : 80;
+                } else if (target.startsWith("/")) {
+                    // origin-form: host comes from host header
+                    path = target;
+                    String hostHeader = headers.get("host");
+                    if(hostHeader == null){
+                        System.out.println("No Host Header Present; cannot route request");
+                        return;
+                    }
+
+                    String[] hp = hostHeader.split(":", 2);
+                    host = hp[0];
+                    port = (hp.length == 2) ? Integer.parseInt(hp[1]) : 80;
+                } else {
+                    // Some unrecognizable form
+                    System.out.println("Unrecognized target form: " + target);
+                    return;
+                }
+            }
+
+            System.out.println("ROUTE => " + method + " " + host + ":" + port + " " + path);
 
             System.out.println("["+ threadName +"] --- Incoming Request ---");
 
